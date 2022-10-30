e20b296d3dbdc4e2fdedda9e4b0fe5593eed7fc1
Dmitriy Savinov <drakon7719@gmail.com>
2022-10-30 14:53:51 +0400

    BS-3 Добавил регистрацию пользователя

--- src/main/java/com/example/battleship/service/AuthenticationService.java
+++ src/main/java/com/example/battleship/service/AuthenticationService.java
@@ -1,4 +1,18 @@
 package com.example.battleship.service;
 
+import com.example.battleship.model.User;
+import com.example.battleship.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthenticationService {
+    private final UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public long registration(User user){
+        return userRepository.save(user).getId();
+    }
 }
