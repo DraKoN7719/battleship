a036e55028979cec3aadee7f061aaf7fb6959e74
serega <serega.uz@mail.ru>
2022-11-10 09:44:57 +0400

    BS-3 Добавил регистрацию и авторизацию пользователя

--- src/main/java/com/example/battleship/repository/UserRepository.java
+++ src/main/java/com/example/battleship/repository/UserRepository.java
@@ -2,9 +2,18 @@ package com.example.battleship.repository;
 
 import com.example.battleship.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(nativeQuery = true,
+            value = """
+                           SELECT * FROM "user" WHERE login = :login
+                    """)
+    Optional<User> getUserByLogin(String login);
 
 }
