27ff13ce9105efd9f6939c8b44c41d22dfc03e08
Dmitriy Savinov <drakon7719@gmail.com>
2022-12-22 03:05:31 +0400

    Доработал онлайн игру

--- src/main/java/com/example/battleship/service/schedule/ScheduleClearOldGamesJob.java
+++ src/main/java/com/example/battleship/service/schedule/ScheduleClearOldGamesJob.java
@@ -1,8 +1,8 @@
 package com.example.battleship.service.schedule;
 
-import com.example.battleship.model.Game;
 import com.example.battleship.model.HistoryGame;
 import com.example.battleship.repository.BattleCache;
+import com.example.battleship.service.GameOnlineService;
 import com.example.battleship.service.HistoryGameService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,18 +13,20 @@ import java.time.Instant;
 public class ScheduleClearOldGamesJob {
 
     private final HistoryGameService historyGameService;
+    private final GameOnlineService gameOnlineService;
 
-    public ScheduleClearOldGamesJob(HistoryGameService historyGameService) {
+    public ScheduleClearOldGamesJob(HistoryGameService historyGameService, GameOnlineService gameOnlineService) {
         this.historyGameService = historyGameService;
+        this.gameOnlineService = gameOnlineService;
     }
 
     @Scheduled(cron = "${battleship.clear-timeout}")
     public void clearOldGames() {
-        System.out.println("Очистка в " + Instant.now());
         BattleCache.clearOldGames().forEach(g -> historyGameService.saveGame(new HistoryGame(g.getId(),
                                                                                              g.getPlayer1(),
                                                                                              g.getPlayer2(),
                                                                                              g.getPlayer2(),
                                                                                              Instant.now())));
+        gameOnlineService.deleteOldGames();
     }
 }
--- src/main/java/com/example/battleship/web/GameController.java
+++ src/main/java/com/example/battleship/web/GameController.java
@@ -1,17 +1,15 @@
 package com.example.battleship.web;
 
+import com.example.battleship.exception.SetOrCreateGameException;
 import com.example.battleship.model.dto.GameOnlineDTO;
 import com.example.battleship.model.dto.MessageDTO;
 import com.example.battleship.service.GameOnlineService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.spel.spi.Function;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.socket.config.WebSocketMessageBrokerStats;
@@ -44,36 +42,47 @@ public class GameController {
 
     @MessageMapping("/game")
     public void processMessage(@Payload GameOnlineDTO gameOnlineDTO) {
-        switch (gameOnlineDTO.getStatus()) {
-            case "JOIN": {
-                gameOnlineDTO.setStatus("");
-                gameOnlineService.addGameOnline(gameOnlineDTO);
-                template.convertAndSendToUser(gameOnlineDTO.getId().toString(), "/game",
-                        new MessageDTO("JOIN", gameOnlineDTO.getPlayer1(), "", "", -1, -1));
-                break;
+        try {
+
+            switch (gameOnlineDTO.getStatus()) {
+                case "JOIN": {
+                    gameOnlineDTO.setStatus("");
+                    gameOnlineService.addGameOnline(gameOnlineDTO);
+                    template.convertAndSendToUser(gameOnlineDTO.getId().toString(), "/game",
+                                                  new MessageDTO("JOIN", gameOnlineDTO.getPlayer1(), "", "", -1, -1));
+                    break;
+                }
+                case "JOIN_PLAYER_2": {
+                    gameOnlineDTO.setStatus("START_GAME");
+                    gameOnlineService.setGameOnline(gameOnlineDTO);
+                    template.convertAndSendToUser(gameOnlineDTO.getId().toString(), "/game",
+                                                  new MessageDTO("JOIN_PLAYER_2", gameOnlineDTO.getPlayer2(), "", "",
+                                                                 -1, -1));
+                    gameOnlineService.deleteGameOnline(gameOnlineDTO.getId());
+                    break;
+                }
+                case "MOTION": {
+                    template.convertAndSendToUser(gameOnlineDTO.getId().toString(), "/game",
+                                                  new MessageDTO(gameOnlineService.userHit(gameOnlineDTO),
+                                                                 gameOnlineDTO.getPlayer1() == null ?
+                                                                     gameOnlineDTO.getPlayer2() :
+                                                                     gameOnlineDTO.getPlayer1(),
+                                                                 "", "", gameOnlineDTO.getX(), gameOnlineDTO.getY()));
+                    break;
+                }
+                case "DISCONNECTION": {
+                    System.out.println("DISCONNECTION");
+                    template.convertAndSendToUser(gameOnlineDTO.getId().toString(), "/game",
+                                                  new MessageDTO("DISCONNECTION", gameOnlineDTO.getPlayer1() == null ?
+                                                      gameOnlineDTO.getPlayer2() : gameOnlineDTO.getPlayer1(),
+                                                                 "", "", -1, -1));
+                    gameOnlineService.deleteGame(gameOnlineDTO.getId());
+                    break;
+                }
             }
-            case "JOIN_PLAYER_2": {
-                gameOnlineDTO.setStatus("START_GAME");
-                gameOnlineService.setGameOnline(gameOnlineDTO);
-                template.convertAndSendToUser(gameOnlineDTO.getId().toString(), "/game",
-                        new MessageDTO("JOIN_PLAYER_2", gameOnlineDTO.getPlayer2(), "", "", -1, -1));
-                gameOnlineService.deleteGameOnline(gameOnlineDTO.getId());
-                break;
-            }
-            case "MOTION": {
-                template.convertAndSendToUser(gameOnlineDTO.getId().toString(), "/game",
-                        new MessageDTO(gameOnlineService.userHit(gameOnlineDTO), gameOnlineDTO.getPlayer1() == null ? gameOnlineDTO.getPlayer2() : gameOnlineDTO.getPlayer1(),
-                                "", "", gameOnlineDTO.getX(), gameOnlineDTO.getY()));
-                break;
-            }
-            case "DISCONNECTION": {
-                System.out.println("DISCONNECTION");
-                template.convertAndSendToUser(gameOnlineDTO.getId().toString(), "/game",
-                        new MessageDTO("DISCONNECTION", gameOnlineDTO.getPlayer1() == null ? gameOnlineDTO.getPlayer2() : gameOnlineDTO.getPlayer1(),
-                                "", "", -1, -1));
-                gameOnlineService.deleteGame(gameOnlineDTO.getId());
-                break;
-            }
+        } catch (SetOrCreateGameException e) {
+            template.convertAndSendToUser(gameOnlineDTO.getId().toString(), "/game",
+                                          new MessageDTO("ERROR", e.getPlayerId(), "", "", -1, -1));
         }
 
     }
